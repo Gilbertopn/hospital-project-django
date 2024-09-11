9d6ec709b94de39312bd972d7f26690e70975f26
Gilberto Junior <juninhossj4@gmail.com>
2024-09-11 01:17:14 -0300

    Criação da lógica de Paciente Create

--- app_hospital_consulta/views.py
+++ app_hospital_consulta/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from models import Paciente
 
-# Create your views here.
+def criar_paciente(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        cpf = request.POST.get('cpf')
+        data_nascimento = request.POST.get('data_nascimento')
+        sexo = request.POST.get('sexo')
+        telefone = request.POST.get('telefone')
+        email = request.POST.get('email')
+
+        Paciente.objects.create(
+            nome = nome,
+            cpf = cpf,
+            data_nascimento = data_nascimento,
+            sexo = sexo,
+            telefone = telefone,
+            email = email
+        )
+
+       
+    return render (request, 'paciente/cadastro_paciente')
